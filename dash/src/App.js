00b47c8f8e1a535b4c690951ff35723c9e2f4831
privosite <bedo@privosoft.com>
2018-11-26 22:14:18 -0500

    add sockets

--- dash/src/App.js
+++ dash/src/App.js
@@ -1,11 +1,62 @@
 import React, { Component } from 'react';
 import './App.css';
+import socketIOClient from "socket.io-client";
 
 class App extends Component {
+
+	constructor() {
+		super();
+		this.state = {
+		  response: false,
+		  endpoint: "https://ws-api.iextrading.com/1.0/tops"
+		};
+	}
+
+	componentDidMount() {
+		const { endpoint } = this.state;
+		const socket = socketIOClient(endpoint);
+
+		socket.on(
+			'message',
+			data => {
+				console.log(data);
+				this.setState({ response: data });
+				return;
+			});
+
+		// Connect to the channel
+		socket.on('connect', () => {
+
+			// Subscribe to topics (i.e. appl,fb,aig+)
+			socket.emit('subscribe', 'snap,fb,goog')
+  
+			// Unsubscribe from topics (i.e. aig+)
+			//socket.emit('unsubscribe', 'aig+')
+  		})
+	}
+
 	render() {
+		// askPrice:0
+		// askSize:0
+		// bidPrice:0
+		// bidSize:0
+		// lastSalePrice:6.495
+		// lastSaleSize:5
+		// lastSaleTime:1543265994184
+		// lastUpdated:1543266000000
+		// marketPercent:0.02332
+		// sector:"mediaentertainment"
+		// securityType:"commonstock"
+		// seq:14591
+		// symbol:"SNAP"
+		// volume:232141
+
+		const { response } = this.state;
+		const data = JSON.parse(response);
+
 		return (
 			<div className="App">
-					
+				<b>{data.symbol}</b> {data.lastSalePrice} {data.lastUpdated}
 			</div>
 		);
 	}
